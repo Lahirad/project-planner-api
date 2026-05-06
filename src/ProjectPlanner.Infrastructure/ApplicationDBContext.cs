a434f08b66dd7f0a80884becee9567bed2e56ca4
user <user@DESKTOP-UTFLJUV>
2026-05-06 06:57:15 +0300

    updating-concurrency-exception

--- src/ProjectPlanner.Infrastructure/ApplicationDBContext.cs
+++ src/ProjectPlanner.Infrastructure/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjectPlanner.Application.Exceptions;
 using ProjectPlanner.Domain.Abstractions;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace ProjectPlanner.Infrastructure
 {
-    public sealed class ApplicationDBContext : DbContext , IUnitOfWork
+    public sealed class ApplicationDBContext : DbContext , IUnitofWork
     {
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,8 +19,6 @@ namespace ProjectPlanner.Infrastructure
 
             base.OnModelCreating(modelBuilder);
         }
-
-
         public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             try
@@ -29,7 +28,7 @@ namespace ProjectPlanner.Infrastructure
             }
             catch (DbUpdateConcurrencyException ex)
             {
-                throw new ApplicationException("Concurrency exception occurred.", ex);
+                throw new ConcurrencyException("Concurrency exception occurred.", ex);
             }
         }
     }

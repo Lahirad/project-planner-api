dfac75d85baa32db413b0886b72e2e227d201d89
user <user@DESKTOP-UTFLJUV>
2026-05-06 06:44:06 +0300

    adding-db-context

--- src/ProjectPlanner.Domain/Abstractions/IUnitOfWork.cs
+++ src/ProjectPlanner.Domain/Abstractions/IUnitOfWork.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProjectPlanner.Domain.Abstractions
 {
-    interface IUnitOfWork
+   public  interface IUnitOfWork
     {
         Task<int> SaveChangesAsync(CancellationToken cancellationToken); 
     }
---
+++ src/ProjectPlanner.Infrastructure/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectPlanner.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPlanner.Infrastructure
+{
+    public sealed class ApplicationDBContext : DbContext , IUnitOfWork
+    {
+    }
+}
+
+

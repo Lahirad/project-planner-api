24710ab47bdeb280ccc1263bfc161f907c120f8e
user <user@DESKTOP-UTFLJUV>
2026-05-14 06:54:06 +0300

    adding-task-configurations

---
+++ src/ProjectPlanner.Infrastructure/Configurations/TaskConfigurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPlanner.Infrastructure.Configurations
+{
+    internal sealed class TaskConfigurations : IEntityTypeConfiguration<Task>
+    {
+        public void Configure(EntityTypeBuilder<Task> builder)
+        {
+            builder.ToTable("TASKS");
+            builder.HasKey(x => x.Id);  
+
+            builder.Property(x => x.Id).HasColumnName("ID");
+
+
+        }
+    }
+}
